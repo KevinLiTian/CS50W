56ab27d3c68c3c4cb174359843dcbfe048f25b0e
KevinLiTian <kevin.li20021106@gmail.com>
2022-07-12 13:16:39 -0400

    Implements Search Functionality

--- 1.Django/wiki/encyclopedia/views.py
+++ 1.Django/wiki/encyclopedia/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from markdown2 import markdown
 
 from . import util
@@ -14,10 +14,38 @@ def page(request, title):
     """ Any Wiki Page """
     content = util.get_entry(title)
 
+    # Page Not Found
     if content is None:
         return render(request, "encyclopedia/404.html")
 
+    # Render the corresponding page
     return render(request, "encyclopedia/page.html", {
         "title": title,
         "content": markdown(content)
     })
+
+def search(request):
+    """ Search for a Wiki Page """
+    if request.method == "POST":
+        query = request.POST.get('q').lower()
+        entries = [entry.lower() for entry in util.list_entries()]
+
+        # If query is in entries, redirect to that page
+        if query in entries:
+            return redirect('page', title=query)
+
+        # Check if query is substring of any entries
+        # Reformat the entries
+        candidates = [entry for entry in entries if query in entry]
+        for idx, entry in enumerate(candidates):
+            if entry in ("html", "css"):
+                candidates[idx] = entry.upper()
+            else:
+                candidates[idx] = entry.capitalize()
+
+        # Show all candidates
+        return render(request, "encyclopedia/search.html", {
+            "candidates": candidates
+        })
+
+    return index(request)
