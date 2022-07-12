52daea1931f3e58f742b9ad92f8e16ee1b644dde
KevinLiTian <kevin.li20021106@gmail.com>
2022-07-12 11:36:48 -0400

    Implement first 2 features of Wiki

--- 1.Django/wiki/encyclopedia/views.py
+++ 1.Django/wiki/encyclopedia/views.py
@@ -4,7 +4,19 @@ from . import util
 
 
 def index(request):
+    """ Home Page """
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
 
+def page(request, title):
+    """ Any Wiki Page """
+    content = util.get_entry(title)
+
+    if content is None:
+        return render(request, "encyclopedia/404.html")
+
+    return render(request, "encyclopedia/page.html", {
+        "title": title,
+        "content": util.get_entry(title)
+    })
