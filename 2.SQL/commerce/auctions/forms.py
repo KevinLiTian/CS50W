a2889967653d84b6e2bccdd85372b1440a26b8d4
KevinLiTian <kevin.li20021106@gmail.com>
2022-07-19 15:41:40 -0400

    Style

--- 2.SQL/commerce/auctions/forms.py
+++ 2.SQL/commerce/auctions/forms.py
@@ -7,13 +7,13 @@ class NewListingForm(forms.Form):
     """ Create New Listing """
     title = forms.CharField(max_length=20, label='', required=True,
             widget=forms.TextInput(attrs={'placeholder': "Listing Title",
-            "class": "form-control w-50 m-auto"}))
+            "class": "form-control"}))
     description = forms.CharField(max_length=128, label='', required=True,
         widget=forms.Textarea(attrs={"placeholder":"Listing Description", 
-        "class": "form-control w-50 m-auto"}))
+        "class": "form-control"}))
     price = forms.CharField(label='', required=True,
         widget=forms.NumberInput(attrs={"placeholder":"Starting Price", "step": 0.01, "min":0,
-        "class": "form-control w-50 m-auto"}))
+        "class": "form-control"}))
     url = forms.CharField(label='',
         widget=forms.URLInput(attrs={"placeholder":"Image URL",
-        "class": "form-control w-50 m-auto"}))
+        "class": "form-control"}))
