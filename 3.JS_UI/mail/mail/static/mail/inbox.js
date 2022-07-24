f4d14de0949b19756be2ca197d1fb3e96269cd6a
KevinLiTian <kevin.li20021106@gmail.com>
2022-07-24 14:47:12 -0400

    Style Navbar

--- 3.JS_UI/mail/mail/static/mail/inbox.js
+++ 3.JS_UI/mail/mail/static/mail/inbox.js
@@ -6,6 +6,17 @@ document.addEventListener('DOMContentLoaded', function() {
   document.querySelector('#archived').addEventListener('click', () => load_mailbox('archive'));
   document.querySelector('#compose').addEventListener('click', compose_email);
 
+  const navLinks = document.querySelectorAll('.nav-item');
+  const menuToggle = document.getElementById('navmenu');
+  const bsCollapse = new bootstrap.Collapse(menuToggle, {toggle:false});
+  navLinks.forEach((l) => {
+    l.addEventListener('click', () => { 
+      if (window.innerWidth < 992) {
+        bsCollapse.toggle() }
+      }
+    )  
+  });
+
   // By default, load the inbox
   load_mailbox('inbox');
 });
