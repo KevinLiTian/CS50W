b6e33a4533f9f0ae97d5d95b964068e59e2cecf8
KevinLiTian <kevin.li20021106@gmail.com>
2022-07-29 13:20:08 -0400

    Create Models

--- 4.Testing_CI_CD/network/network/models.py
+++ 4.Testing_CI_CD/network/network/models.py
@@ -1,6 +1,19 @@
+""" Database """
+
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
 class User(AbstractUser):
-    pass
+    """ All Users """
+
+class Post(models.Model):
+    """ All Posts """
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
+    content = models.TextField()
+    likes = models.IntegerField()
+
+class Follow(models.Model):
+    """ All User Followingsm (user1 follows user2) """
+    user1 = models.ForeignKey(User, on_delete=models.CASCADE, related_name='following_others')
+    user2 = models.ForeignKey(User, on_delete=models.CASCADE)
